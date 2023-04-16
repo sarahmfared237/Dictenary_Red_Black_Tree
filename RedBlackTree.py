e08e4a2dc9f03e1171e0d914fcef61f58af6428a
Abdallah <abdalla.elsaid25@gmail.com>
2023-04-16 16:30:06 +0200

    Dictionary

--- RedBlackTree.py
+++ RedBlackTree.py
@@ -18,11 +18,17 @@ class RedBlackTree:
     def inc_size(self):
         self.__size += 1
 
-    def get_height(self) -> Node:
-        return self.__height
+    def __get_height_helper(self, root:Node) -> Node:
+        if root is Nil.get_instance():
+            return 0
 
-    def inc_height(self, root:Node):
-        self.__height += 1
+        left_height = self.__get_height_helper(root.get_left_node())
+        right_height = self.__get_height_helper(root.get_right_node())
+
+        return max(left_height, right_height) + 1
+    
+    def get_height(self) -> Node:
+        return self.__get_height_helper(self.get_root())
 
     def __search_helper(self, data, root:Node):
         
@@ -84,7 +90,7 @@ class RedBlackTree:
 
         y.set_parent(x)
     
-    def __insert_helper(self, data, root:Node, parent:Node):
+    def __insert_helper(self, data, root:Node, parent:Node, height):
         if root == Nil.get_instance():
             new_node = Node(data, 'R')
             new_node.set_parent(parent)
@@ -100,12 +106,12 @@ class RedBlackTree:
             self.__fixup(new_node)
   
         elif data > root.get_value():
-            self.__insert_helper(data, root.get_right_node(), root)
+            self.__insert_helper(data, root.get_right_node(), root, height + 1)
         else:
-            self.__insert_helper(data, root.get_left_node(), root)
+            self.__insert_helper(data, root.get_left_node(), root, height + 1)
         
     def insert(self, data):            
-        self.__insert_helper(data, self.get_root(), Nil.get_instance())
+        self.__insert_helper(data, self.get_root(), Nil.get_instance(), 0)
         self.inc_size()
 
     def __fixup_case_one(self, node:Node):
@@ -126,9 +132,9 @@ class RedBlackTree:
         node.get_grand_parent().change_color('R')
 
         if node.get_grand_parent().get_left_node() == node.get_parent():
-            self.right_rotate(node.get_parent())
+            self.right_rotate(node.get_grand_parent())
         else:
-            self.left_rotate(node.get_parent())
+            self.left_rotate(node.get_grand_parent())
 
     def __fixup(self, node:Node):
         while node.get_parent().get_color() == 'R':
@@ -167,17 +173,9 @@ class RedBlackTree:
             self.print_tree_helper(node.get_right_node(), indent, True)
 
 
-rb = RedBlackTree()
-rb.insert(4)
-rb.print_red_black_tree()
-
-rb.insert(2)
-rb.print_red_black_tree()
-
-rb.insert(1)
-
-rb.insert(3)
-
-
-
-
+# rb = RedBlackTree()
+# rb.insert(4)
+# rb.insert(2)
+# rb.insert(1)
+# rb.insert(3)
+# rb.print_red_black_tree()
---
+++ dictionary.py
@@ -0,0 +1,8 @@
+from RedBlackTree import RedBlackTree
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
