d247ae9dcea942950e3cab05908ef704c497b1c8
Nada Mostafa <nm647152@gmail.com>
2023-04-17 03:31:20 +0200

    modification

--- Dictionary.py
+++ Dictionary.py
@@ -5,19 +5,22 @@ class Dictionary:
         self.__rbTree = RedBlackTree()
 
     def load(self, filename):
-        with open(f'{filename}.txt', 'r') as file:
+        with open(f'C:/Users/nadam/OneDrive/Desktop/rb/Dictenary_Red_Black_Tree/{filename}.txt', 'r') as file:
             lines = file.readlines()
             for line in lines:
                 self.__rbTree.insert(line[:-1])
     
     def look_up(self, word):
-        return self.__rbTree.search(word)
+        return self.__rbTree.search(word) 
+
 
     def insert_word(self, word):
         if self.look_up(word):
             print("ERROR: Word already in the dictionary!")
         else:
             self.__rbTree.insert(word)
+            print(self.size())
+            print(self.rbTree_height())
         self.__rbTree.print_red_black_tree()
 
     def size(self):
--- test.py
+++ test.py
@@ -2,8 +2,19 @@ from Dictionary import Dictionary
 
 def main():
     dic = Dictionary()
-    dic.load("words")
+    dic.load("dictionary")
     size_validation(dic)
+    while (input("If you want to insert press 1\n")=="1"):
+        word=input("Enter the word you want to insert\n")
+        dic.insert_word(word)
+        size_validation(dic)
+    while(input("If you want to look up a word press 2\n")=="2"):
+        look=input("Enter the word you want to look for\n")   
+        if(dic.look_up(look)):
+            print("Yes")
+        else:
+            print("No") 
+        
 
 
 def size_validation(dic:Dictionary):
